df9ba39a95cc0e48d813ed8137e5be557c7c26cb
aguldstrand <golfin85@gmail.com>
2013-10-14 22:16:25 +0200

    return all tracked items by date

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+var async = require('async');
 var Logger = require('../utils/logger');
 
 exports.init = function (instanceId, app) {
@@ -15,6 +17,72 @@ exports.init = function (instanceId, app) {
 	});
 
 	app.get('/api/stats/:date', function(request, response) {
-		response.end();
+
+		var basePath = 'logs';
+
+		function filter(path, callback) {
+
+			if(!path || path.indexOf(request.params.date) !== 0) {
+				callback(false);
+				return;
+			}
+
+			fs.stat(basePath + '/' + path, function(err, stats) {
+				if(err) {
+					callback(false);
+					return;
+				}
+
+				callback(stats.isFile());
+			});
+		}
+
+		function readFile(path, callback) {
+			fs.readFile(basePath + '/' + path, {
+				encoding: 'utf-8'
+			}, callback);
+		}
+
+		async.waterfall([
+			function(callback) {
+				fs.readdir(basePath, callback);
+			},
+			function(files, callback) {
+				async.filter(files, filter, function(files) {
+					callback(null, files);
+				});
+			},
+			function(files, callback) {
+				async.map(files, readFile, callback);
+			},
+			function(fileContents, callback) {
+				var outp = [];
+				for (var i = 0; i < fileContents.length; i++) {
+					var fileContent = fileContents[i];
+
+					var lines = fileContent.split('\n');
+					for (var j = 0; j < lines.length; j++) {
+						if(lines[j].length) {
+							var data = JSON.parse(lines[j]);
+							outp.push(data);
+						}
+					};
+				};
+
+				outp.sort(function(a, b) {
+					return a.t.localeCompare(b.t);
+				});
+
+				callback(null, outp);
+			}],
+			function(err, datas) {
+				if(err) {
+					response.status(500);
+					response.send(err);
+					return;
+				}
+
+				response.send(datas);
+			});
 	});
 };
